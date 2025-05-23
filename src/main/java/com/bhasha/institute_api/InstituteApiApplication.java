aaaf189bb6836229dd0fadb50432e7ce89f9cc5b
Sahan Devaka <sahandevaka9@gmail.com>
2025-05-23 10:01:52 +0530

    Initialize Spring Boot project with Maven Wrapper and essential configurations

---
+++ src/main/java/com/bhasha/institute_api/InstituteApiApplication.java
@@ -0,0 +1,13 @@
+package com.bhasha.institute_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstituteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstituteApiApplication.class, args);
+	}
+
+}
